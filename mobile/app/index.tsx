bc19b7c62f6abfb965348b6769ab7c7ae4d93fb1
hoang cuong <vancuong25112003@gmail.com>
2025-03-19 05:17:27 +0700

    set up mobile

---
+++ mobile/app/index.tsx
@@ -0,0 +1,17 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Link href='/(auth)/signup'>Signup</Link>
+      <Link href='/(auth)/login'>Login</Link>
+    </View>
+  );
+}
