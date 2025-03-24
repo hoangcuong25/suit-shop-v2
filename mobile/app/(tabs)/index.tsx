8d5f0ee879233af60307df8d74d864334aa771cf
hoang cuong <vancuong25112003@gmail.com>
2025-03-24 19:01:46 +0700

    update home

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,6 +1,11 @@
 import { Link } from "expo-router";
-import { Image, Text, View, ScrollView } from "react-native";
-const imageHero = require('@/assets/images/spring_2025_hero.jpg');
+import { Image, Text, View, ScrollView, Pressable } from "react-native";
+const imageHero = require('@/assets/images/spring_2025_hero.jpg')
+const image1 = require('@/assets/images/spotlight-collection-essentials.jpg')
+const image2 = require('@/assets/images/spotlight-collection-mens-2.jpg')
+const image3 = require('@/assets/images/spotlight-collection-prom.jpg')
+const image4 = require('@/assets/images/spotlight-collection-womens.jpg')
+const fullImage = require('@/assets/images/tan-magazine.jpg')
 import styles from "@/styles/home.styles";
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
@@ -60,6 +65,60 @@ export default function Index() {
               <Text>Broad size range, easy fit tools, no</Text>
               <Text>measurements required</Text>
             </View>
+          </View>
+        </View>
+
+        <View style={{ flexDirection: 'column', gap: 15, }}>
+          <View style={{ position: 'relative' }}>
+            <Image source={image1} style={{ width: 400, height: 400 }} />
+            <View style={styles.textImage}>
+              <Text style={{ fontSize: 30, fontWeight: 600, color: 'white' }}>Suiting Essentials</Text>
+              <Text style={{ textDecorationLine: 'underline', color: 'white' }}>SHOP NOW</Text>
+            </View>
+          </View>
+
+          <View style={{ position: 'relative' }}>
+            <Image source={image2} style={{ width: 400, height: 400 }} />
+            <View style={styles.textImage}>
+              <Text style={{ fontSize: 30, fontWeight: 600, color: 'white' }}>Men&apos;s Suits & Tuxedos</Text>
+              <Text style={{ textDecorationLine: 'underline', color: 'white' }}>SHOP NOW</Text>
+            </View>
+          </View>
+
+          <View style={{ position: 'relative' }}>
+            <Image source={image3} style={{ width: 400, height: 400 }} />
+            <View style={styles.textImage}>
+              <Text style={{ fontSize: 30, fontWeight: 600, color: 'white' }}>Prom & Homecoming</Text>
+              <Text style={{ textDecorationLine: 'underline', color: 'white' }}>SHOP NOW</Text>
+            </View>
+          </View>
+
+          <View style={{ position: 'relative' }}>
+            <Image source={image4} style={{ width: 400, height: 400 }} />
+            <View style={styles.textImage}>
+              <Text style={{ fontSize: 30, fontWeight: 600, color: 'white' }}>Women&apos;s Suits & Tuxedos</Text>
+              <Text style={{ textDecorationLine: 'underline', color: 'white' }}>SHOP NOW</Text>
+            </View>
+          </View>
+        </View>
+
+        <View>
+          <Text style={{ fontSize: 20, fontWeight: 600, borderBottomWidth: 1, paddingBottom: 10 }}>Interesting products:</Text>
+        </View>
+
+        <View style={{ position: 'relative' }}>
+          <Image source={fullImage} style={{ width: 400, height: 300 }} />
+
+          <View style={styles.viewFullImage}>
+            <Text style={{ fontSize: 25, fontWeight: 600, color: 'white', fontFamily: 'sans-serif' }}>
+              Street-stopping: {"\n"}
+              Tan takes noteworthy {"\n"}
+              neutrals in stride
+            </Text>
+
+            <Pressable style={{ marginTop: 12, borderWidth: 1, borderColor: 'white', padding: 10, width: 200, alignItems: 'center' }}>
+              <Text style={{ color: 'white', fontSize: 18 }}>FIND YOUR LOOK</Text>
+            </Pressable>
           </View>
         </View>
       </View>
--- mobile/styles/home.styles.ts
+++ mobile/styles/home.styles.ts
@@ -11,6 +11,22 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 350,
         borderRadius: 10,
+    },
+    textImage: {
+        position: 'absolute',
+        bottom: 20,
+        flexDirection: 'column',
+        gap: 10,
+        alignItems: 'center',
+        width: '100%'
+    },
+    viewFullImage: {
+        position: 'absolute',
+        bottom: 20,
+        flexDirection: 'column',
+        gap: 10,
+        width: '100%',
+        left: 15,
     }
 });
 
